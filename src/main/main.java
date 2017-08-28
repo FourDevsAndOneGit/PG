86430a66b232f39a854c4f7bb03a0c4341a9a401
RoniOe <ronicarlos.oe@hotmail.com>
2017-08-28 16:50:18 -0300

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,19 @@
+package main;
+
+public class main {
+	
+	/**
+	 * Main will be the starting point of the entire program.
+	 * The starting point will be divided into 2, light version (where it will be a part made entirely in the terminal), 
+	 * and full version (where interface will be used).
+	 * Main will have a version choice system where the user can choose which version to use.
+	 * Main will have 2 methods, one will use EventQueue from awt with run method for the full version, and another will 
+	 * start directly to other methods in other packages in which to start light version.
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
